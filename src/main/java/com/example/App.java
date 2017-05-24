71f99c635e6384a3eb29ad78baa8799a58bc7b6d
Srikanth <srikanth.jupally@valuelabs.com>
2017-05-24 14:08:06 +0530

    commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import com.example.bean.BookProxy;
 import com.example.entity.Book;
+import com.google.gson.Gson;
+import com.univocity.parsers.common.processor.BeanWriterProcessor;
 import com.univocity.parsers.fixed.FixedWidthFieldLengths;
 import com.univocity.parsers.fixed.FixedWidthWriter;
 import com.univocity.parsers.fixed.FixedWidthWriterSettings;
-import net.sf.cglib.beans.BeanGenerator;
-import net.sf.cglib.core.NamingPolicy;
-import net.sf.cglib.core.Predicate;
+import org.apache.commons.beanutils.BeanUtils;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.lang.reflect.Field;
@@ -19,7 +21,7 @@ import java.util.Map;
  * Created by Srikanth on 5/23/2017.
  */
 class App {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         BooksManager manager = new BooksManager();
         List<Book> bookList = manager.getBooks();
         List<String> keyList = new ArrayList<String>();
@@ -27,39 +29,87 @@ class App {
         keyList.add("nirc");
         keyList.add("price");
         Map<String, List> keyMap = new HashMap<String, List>();
-        for (String key: keyList  ){
+        /*for (String key: keyList  ){
             List result = processObject(key, bookList);
             keyMap.put(key,result);
-        }
-        for (int i = 0; i < bookList.size(); i++) {
-            String titleList = (String) keyMap.get("title").get(i);
-            String nircList = (String) keyMap.get("nirc").get(i);
-            Double priceList = (Double) keyMap.get("price").get(i);
+        }*/
+        App app = new App();
+        List<BookProxy> proxyList = new ArrayList<BookProxy>();
+        for (Book book : bookList) {
+            JSONObject jsonObject = app.procss(keyList, book, new BookProxy());
+            Gson gson = new Gson();
+            BookProxy bookProxy = gson.fromJson(jsonObject.toString(), BookProxy.class);
+            proxyList.add(bookProxy);
         }
 
 
-        writeIntoFile(keyMap,bookList);
+        writeIntoFile(keyList, proxyList);
 
     }
 
-    private static void writeIntoFile(Map<String, List> keyMap, List<Book> bookList) throws FileNotFoundException {
-        ByteArrayOutputStream fixedWidthResult = new ByteArrayOutputStream();
+    private JSONObject procss(List<String> keyList, Book book, BookProxy proxy) throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        for (String key : keyList) {
+            Class<?> clazz = book.getClass();
+            Object value = null;
+            try {
+                Field field = clazz.getDeclaredField(key);
+                field.setAccessible(true);
+                value = field.get(book);
+                jsonObject.put(key, value);
+            } catch (NoSuchFieldException e) {
+                value = null;
+            } catch (IllegalAccessException e) {
+                value = null;
+            }
+        }
+        return jsonObject;
+    }
 
-        // CsvWriter (and all other file writers) work with an instance of java.io.Writer
-        Writer outputWriter = new OutputStreamWriter(fixedWidthResult);
+    Object instantiate(List<Object> args, String className) throws Exception {
+        // Load the class.
+        Class<?> clazz = Class.forName(className);
+        Field[] fields = clazz.getDeclaredFields();
+        for (Object obj : args) {
+            for (Field f : fields) {
+                if (!f.isAccessible()) {
+                    f.setAccessible(true);
+                    Class<?> type = f.getType();
+
+                    if (type.equals(Double.class)) {
+                        f.set(clazz, obj); //Set Default value
+                    } else if (type.equals(String.class)) {
+                        f.set(clazz, obj);
+                    }
+                    f.setAccessible(false);
+                }
+            }
+        }
+        return clazz;
+    }
+
+    private static void writeIntoFile(List<String> keyList, List<BookProxy> bookList) throws FileNotFoundException {
         FixedWidthFieldLengths lengths = new FixedWidthFieldLengths(20, 20, 35);
         FixedWidthWriterSettings settings = new FixedWidthWriterSettings(lengths);
 
         // Any null values will be written as ?
         settings.setNullValue("?");
 
+        // Creates a BeanWriterProcessor that handles annotated fields in the TestBean class.
+        settings.setRowWriterProcessor(new BeanWriterProcessor<BookProxy>(BookProxy.class));
+
         // Sets the file headers so the writer knows the correct order when writing values taken from a TestBean instance
-        settings.setHeaders("amount", "pending", "date", "quantity", "comments");
+        settings.setHeaders("title", "nirc", "price");
 
         // Creates a writer with the above settings;
         FixedWidthWriter writer = new FixedWidthWriter(new FileOutputStream(new File("output.txt")), settings);
-        for (int i = 0; i < bookList.size(); i++) {
-            writer.writeRow(keyMap.get("title").get(i), keyMap.get("nirc").get(i), keyMap.get("price").get(i));
+
+        // Writes the headers specified in the settings
+        writer.writeHeaders();
+
+
+        for (BookProxy bean:bookList) {
+            writer.processRecord(bean);
         }
 
         writer.close();
@@ -67,7 +117,7 @@ class App {
 
     private static List processObject(String key, List objList) {
         List<Object> list = new ArrayList<Object>();
-        for (Object obj: objList) {
+        for (Object obj : objList) {
             Class<?> clazz = obj.getClass();
             Object value = null;
             try {
